a50ed6eecddedc16ab9f1f6e3e4d9d17b158063e
Jonas De Vuyst <jdv@foobar.be>
2015-01-03 11:01:56 +0100

    Updated project.clj

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
 (defproject fletching-macros "0.9"
   :description "Clojure macros that complement the clojure.core threading (arrow) macros"
-  :url "https://github.com/jdevuyst/fletching"
+  :url "https://github.com/jdevuyst/fletching-macros"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]]
